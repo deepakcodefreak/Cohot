11b409ebe0447487b37d42d433f4457489202667
Deepak Chawla <deepakchawla@Deepaks-MacBook-Pro.local>
2024-03-09 16:14:47 +0530

    feat(Serverless): serverless framework heno

---
+++ hono-app/src/index.ts
@@ -0,0 +1,11 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.post('/', async (c) => {
+  const body = await c.req.json()
+  console.log(body);
+  return c.json(body)
+})
+
+export default app
